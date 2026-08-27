5f308600144f59cd3e7650e5582d9dfd599b9e09
SlothBusta1 <ochalskinicholas@gmail.com>
2026-08-26 22:54:19 -0400

    Fix icon file path in service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = "steady-shell-v1";
-const SHELL_FILES = ["/", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
-
+const SHELL_FILES = ["/", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)));
   self.skipWaiting();
